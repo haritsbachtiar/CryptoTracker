3e152f906a36b5bfaabdf7b626c4c06b5cc17902
Ahmad Harits <ahmad.harits@tiket.com>
2024-10-20 13:03:17 +0700

    Setup Network Utility

---
+++ app/src/main/java/com/plcoding/cryptotracker/core/data/networking/safeCall.kt
@@ -0,0 +1,25 @@
+package com.plcoding.cryptotracker.core.data.networking
+
+import com.plcoding.cryptotracker.core.domain.util.NetworkError
+import com.plcoding.cryptotracker.core.domain.util.Result
+import io.ktor.client.statement.HttpResponse
+import io.ktor.util.network.UnresolvedAddressException
+import kotlinx.coroutines.ensureActive
+import kotlin.coroutines.coroutineContext
+
+//
+suspend inline fun <reified T> safeCall(
+    execute: () -> HttpResponse
+): Result<T, NetworkError> {
+    val response = try {
+        execute()
+    } catch (e: UnresolvedAddressException) {
+        return Result.Error(NetworkError.NO_INTERNET)
+    } catch (e: UnresolvedAddressException) {
+        return Result.Error(NetworkError.SERIALIZATION)
+    } catch (e: UnresolvedAddressException) {
+        coroutineContext.ensureActive()
+        return Result.Error(NetworkError.UNKNOWN)
+    }
+    return responseToResult(response)
+}
